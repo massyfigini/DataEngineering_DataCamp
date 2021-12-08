66c657b4ae235efc1d94274d7f5d5bb5bf6ac5b0
massyfigini <massimiliano.figini@gmail.com>
2021-12-08 18:59:10 +0100

    data cleaning and munging on the command line

--- Data_Processing_in_Shell.sh
+++ Data_Processing_in_Shell.sh
@@ -61,3 +61,138 @@ wget --limit-rate=2500k -i url_list.txt
 
 # Take a look at all files downloaded
 ls
+
+
+
+
+#################################################
+# Data Cleaning and Munging on the Command Line #
+#################################################
+
+### Installation and documentation for csvkit
+
+# Upgrade csvkit using pip  
+pip install --upgrade csvkit
+
+# Print manual for in2csv
+in2csv -h
+
+# Print manual for csvlook
+csvlook -help
+
+
+### Converting and previewing data with csvkit
+
+# Use ls to find the name of the zipped file
+ls
+
+# Use Linux's built in unzip tool to unpack the zipped file 
+unzip SpotifyData.zip
+
+# Check to confirm name and location of unzipped file
+ls
+
+
+### File conversion and summary statistics with csvkit
+
+# Check to confirm name and location of the Excel data file
+ls
+
+# Convert sheet "Worksheet1_Popularity" to CSV
+in2csv SpotifyData.xlsx --sheet "Worksheet1_Popularity" > Spotify_Popularity.csv
+
+# Check to confirm name and location of the new CSV file
+ls
+
+# Print high level summary statistics for each column
+csvstat Spotify_Popularity.csv 
+
+# Check to confirm name and location of the Excel data file
+ls
+
+# Convert sheet "Worksheet2_MusicAttributes" to CSV
+in2csv SpotifyData.xlsx --sheet "Worksheet2_MusicAttributes" > Spotify_MusicAttributes.csv
+
+# Check to confirm name and location of the new CSV file
+ls
+
+# Print preview of Spotify_MusicAttributes
+csvlook Spotify_MusicAttributes.csv
+
+
+### Printing column headers with csvkit
+
+# Check to confirm name and location of data file
+ls
+
+# Print a list of column headers in data file 
+csvcut -n Spotify_MusicAttributes.csv
+
+
+### Filtering data by column with csvkit
+
+# Print a list of column headers in the data 
+csvcut -n Spotify_MusicAttributes.csv
+
+# Print the first column, by position
+csvcut -c 1 Spotify_MusicAttributes.csv
+
+# Print the first, third, and fifth column, by position
+csvcut -c 1,3,5 Spotify_MusicAttributes.csv
+
+# Print the first column, by name
+csvcut -c "track_id" Spotify_MusicAttributes.csv
+
+# Print the track id, song duration, and loudness, by name 
+csvcut -c "track_id","duration_ms","loudness" Spotify_MusicAttributes.csv
+
+
+### Filtering data by row with csvkit
+
+# Print a list of column headers in the data 
+csvcut -n Spotify_MusicAttributes.csv
+
+# Filter for row(s) where track_id = 118GQ70Sp6pMqn6w1oKuki
+csvgrep -c "track_id" -m 118GQ70Sp6pMqn6w1oKuki Spotify_MusicAttributes.csv
+
+
+### Stacking files with csvkit
+
+# Stack the two files and save results as a new file
+csvstack SpotifyData_PopularityRank6.csv SpotifyData_PopularityRank7.csv > SpotifyPopularity.csv
+
+# Preview the newly created file 
+csvlook SpotifyPopularity.csv
+
+
+### Chaining commands using operators
+
+# If csvlook succeeds, then run csvstat 
+csvlook Spotify_Popularity.csv && csvstat Spotify_Popularity.csv
+
+# Use the output of csvsort as input to csvlook
+csvsort -c 2 Spotify_Popularity.csv | csvlook
+
+# Take top 15 rows from sorted output and save to new file
+csvsort -c 2 Spotify_Popularity.csv | head -n 15 > Spotify_Popularity_Top15.csv
+
+# Preview the new file 
+csvlook Spotify_Popularity_Top15.csv
+
+
+### Data processing with csvkit
+
+# Convert the Spotify201809 tab into its own csv file 
+in2csv Spotify_201809_201810.xlsx --sheet "Spotify201809" > Spotify201809.csv
+
+# Check to confirm name and location of data file
+ls
+
+# Preview file preview using a csvkit function
+csvlook Spotify201809.csv
+
+# Create a new csv with 2 columns: track_id and popularity
+csvcut -c "track_id","popularity" Spotify201809.csv > Spotify201809_subset.csv
+
+# While stacking the 2 files, create a data source column
+csvstack -g "Sep2018","Oct2018" Spotify201809_subset.csv Spotify201810_subset.csv > Spotify_all_rankings.csv
