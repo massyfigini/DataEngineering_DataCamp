8e6b0f7171cee424525c2610654c18bc0f485fb0
massyfigini <massimiliano.figini@gmail.com>
2021-12-08 17:52:36 +0100

    downloading data on the command line

---
+++ Data_Processing_in_Shell.sh
@@ -0,0 +1,63 @@
+########################################
+# Downloading Data on the Command Line #
+########################################
+
+### Downloading single file using curl
+
+# Use curl to download the file from the redirected URL
+curl -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
+
+# Download and rename the file in the same step
+curl -o Spotify201812.zip -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
+
+
+### Downloading multiple files using curl
+
+# Download all 100 data files
+curl -O https://s3.amazonaws.com/assets.datacamp.com/production/repositories/4180/datasets/files/datafile[001-100].txt
+
+# Print all downloaded files to directory
+ls datafile*.txt
+
+
+### Downloading single file using wget
+
+# Fill in the two option flags 
+wget -c -b https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
+
+# Verify that the Spotify file has been downloaded
+ls 
+
+# Preview the log file 
+cat wget-log
+
+
+### Creating wait time using Wget
+
+# View url_list.txt to verify content
+cat url_list.txt
+
+# Create a mandatory 1 second pause between downloading all files in url_list.txt
+wget --wait=1 -i url_list.txt
+
+# Take a look at all files downloaded
+ls
+
+
+### Data downloading with Wget and curl
+
+# Use curl, download and rename a single file from URL
+curl -o Spotify201812.zip -L https://assets.datacamp.com/production/repositories/4180/datasets/eb1d6a36fa3039e4e00064797e1a1600d267b135/201812SpotifyData.zip
+
+# Unzip, delete, then re-name to Spotify201812.csv
+unzip Spotify201812.zip && rm Spotify201812.zip
+mv 201812SpotifyData.csv Spotify201812.csv
+
+# View url_list.txt to verify content
+cat url_list.txt
+
+# Use Wget, limit the download rate to 2500 KB/s, download all files in url_list.txt
+wget --limit-rate=2500k -i url_list.txt
+
+# Take a look at all files downloaded
+ls
