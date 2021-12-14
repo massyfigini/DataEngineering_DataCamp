a6b55e7b06c6c521d0f58d754d1c5a3b83a40640
massyfigini <massimiliano.figini@gmail.com>
2021-12-14 16:15:56 +0100

    variables in bash scripting

---
+++ Introduction_to_Bash_Scripting.sh
@@ -0,0 +1,44 @@
+###############################
+# Variables in Bash Scripting #
+###############################
+
+### Creating an array
+
+# Create a normal array with the mentioned elements
+capital_cities=("Sydney" "New York" "Paris")
+
+# Create a normal array with the mentioned elements using the declare method
+declare -a capital_cities
+
+# Add (append) the elements
+capital_cities+=("Sydney")
+capital_cities+=("New York")
+capital_cities+=("Paris")
+
+# Print out the entire array
+echo ${capital_cities[@]}
+
+# Print out the array length
+echo ${#capital_cities[@]}
+
+
+### Creating associative arrays
+
+# Create empty associative array
+declare -A model_metrics
+
+# Add the key-value pairs
+model_metrics[model_accuracy]=98
+model_metrics[model_name]="knn"
+model_metrics[model_f1]=0.82
+
+# Declare associative array with key-value pairs on one line
+declare -A model_metrics=([model_accuracy]=98 [model_name]="knn" [model_f1]=0.82)
+
+# Print out the entire array
+echo ${model_metrics[@]}
+
+# Print out just the keys
+echo ${!model_metrics[@]}
+
+
