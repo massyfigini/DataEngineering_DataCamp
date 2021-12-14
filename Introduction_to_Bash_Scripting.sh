954fe897a108dff5ad43f6f6f49d2909b70983bd
massyfigini <massimiliano.figini@gmail.com>
2021-12-14 17:04:30 +0100

    functions and automations

--- Introduction_to_Bash_Scripting.sh
+++ Introduction_to_Bash_Scripting.sh
@@ -42,3 +42,19 @@ echo ${model_metrics[@]}
 echo ${!model_metrics[@]}
 
 
+
+
+############################
+# Functions and Automation #
+############################
+
+### Creating cronjobs
+
+# Create a schedule for 30 minutes past 2am every day
+30 2 * * * bash script1.sh
+
+# Create a schedule for every 15, 30 and 45 minutes past the hour
+15,30,45 * * * * bash script2.sh
+
+# Create a schedule for 11.30pm on Sunday evening, every week
+30 23 * * 0 bash script3.sh
