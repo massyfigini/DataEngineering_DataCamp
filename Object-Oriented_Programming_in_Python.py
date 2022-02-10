c08ceff0524b7184442683b21b1732fc8d4d814d
massyfigini <massimiliano.figini@gmail.com>
2022-02-10 15:41:45 +0100

    best practices of class design

--- Object-Oriented_Programming_in_Python.py
+++ Object-Oriented_Programming_in_Python.py
@@ -691,3 +691,134 @@ class Employee:
       
     else:  
       self.salary += amount
+
+
+
+
+##################################
+# Best Practices of Class Design #
+##################################
+
+### Square and rectangle
+
+# 1
+# Define a Rectangle class
+class Rectangle:
+    def __init__(self,h,w):
+        self.h = h
+        self.w = w
+
+# Define a Square class
+class Square(Rectangle):
+    def __init__(self,w):
+        self.w = w 
+        self.h = w
+
+# 2
+class Rectangle:
+    def __init__(self, w,h):
+      self.w, self.h = w,h
+      
+# Define set_h to set h       
+    def set_h(self, h):
+      self.h = h
+
+# Define set_w to set w
+    def set_w(self, w):
+      self.w = w   
+      
+class Square(Rectangle):
+    def __init__(self, w):
+      self.w, self.h = w, w 
+      
+# Define set_h to set w and h 
+    def set_h(self, h):
+      self.h = h
+      self.w = h
+      
+# Define set_w to set w and h 
+    def set_w(self, w):
+      self.w = w   
+      self.h = w  
+
+
+### Using internal attributes
+
+# Add class attributes for max number of days and months
+class BetterDate:
+    
+    _MAX_DAYS = 30
+    _MAX_MONTHS = 12
+    
+    def __init__(self, year, month, day):
+        self.year, self.month, self.day = year, month, day
+        
+    @classmethod
+    def from_str(cls, datestr):
+        year, month, day = map(int, datestr.split("-"))
+        return cls(year, month, day)
+    
+    # Add _is_valid() checking day and month values
+    def _is_valid(self):
+        return (self.day <= BetterDate._MAX_DAYS) and \
+            (self.month <= BetterDate._MAX_MONTHS)
+    
+bd1 = BetterDate(2020, 4, 30)
+print(bd1._is_valid())
+
+bd2 = BetterDate(2020, 6, 45)
+print(bd2._is_valid())
+
+
+### Create and set properties
+
+class Customer:
+    def __init__(self, name, new_bal):
+        self.name = name
+        if new_bal < 0:
+           raise ValueError("Invalid balance!")
+        self._balance = new_bal  
+
+    # Add a decorated balance() method returning _balance        
+    @property
+    def balance(self):
+        return self._balance
+
+    # Add a setter balance() method
+    @balance.setter
+    def balance(self, new_bal):
+        # Validate the parameter value
+        if new_bal < 0:
+           raise ValueError("Invalid balance!")
+        self._balance = new_bal
+        print("Setter method called")
+
+# Create a Customer        
+cust = Customer("Belinda Lutz", 2000)
+cust.balance = 3000
+print(cust.balance)
+
+
+### Read-only properties
+
+import pandas as pd
+from datetime import datetime
+
+# MODIFY the class to use _created_at instead of created_at
+class LoggedDF(pd.DataFrame):
+    def __init__(self, *args, **kwargs):
+        pd.DataFrame.__init__(self, *args, **kwargs)
+        self._created_at = datetime.today()
+    
+    def to_csv(self, *args, **kwargs):
+        temp = self.copy()
+        temp["created_at"] = self._created_at
+        pd.DataFrame.to_csv(temp, *args, **kwargs)   
+    
+    # Add a read-only property: _created_at
+    @property  
+    def created_at(self):
+        return self._created_at
+
+# Instantiate a LoggedDF called ldf
+ldf = LoggedDF({"col1": [1,2], "col2":[3,4]}) 
