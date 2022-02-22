6a7e401563dba2a14446da616a8a0c8fd02deda9
massyfigini <massimiliano.figini@gmail.com>
2022-02-22 12:09:44 +0100

    building production pipelines in Airflow

--- Introduction_to_Airflow_in_Python.py
+++ Introduction_to_Airflow_in_Python.py
@@ -194,3 +194,29 @@ email_report = EmailOperator(
 
 # Set the email task to run after the report is generated
 email_report << generate_report
+
+
+
+
+############################################
+# Building production pipelines in Airflow #
+############################################
+
+### Define a BranchPythonOperator
+
+# Create a function to determine if years are different
+def year_check(**kwargs):
+    current_year = int(kwargs['ds_nodash'][0:4])
+    previous_year = int(kwargs['prev_ds_nodash'][0:4])
+    if current_year == previous_year:
+        return 'current_year_task'
+    else:
+        return 'new_year_task'
+
+# Define the BranchPythonOperator
+branch_task = BranchPythonOperator(task_id='branch_task', dag=branch_dag,
+                                   python_callable=year_check, provide_context=True)
+
+# Define the dependencies
+branch_dag >> current_year_task
+branch_dag >> new_year_task
