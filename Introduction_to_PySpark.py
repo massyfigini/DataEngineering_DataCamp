cdb7b63dae173d269fc0a4c3c7a24b29ff423f76
massyfigini <massimiliano.figini@gmail.com>
2022-02-28 11:01:28 +0100

    model tuning and selection

--- Introduction_to_PySpark.py
+++ Introduction_to_PySpark.py
@@ -295,3 +295,70 @@ piped_data = flights_pipe.fit(model_data).transform(model_data)
 
 # Split the data into training and test sets
 training, test = piped_data.randomSplit([.6, .4])
+
+
+
+
+##############################
+# Model tuning and selection #
+##############################
+
+### Create the modeler
+
+# Import LogisticRegression
+from pyspark.ml.classification import LogisticRegression
+
+# Create a LogisticRegression Estimator
+lr = LogisticRegression()
+
+
+### Create the evaluator
+
+# Import the evaluation submodule
+import pyspark.ml.evaluation as evals
+
+# Create a BinaryClassificationEvaluator
+evaluator = evals.BinaryClassificationEvaluator(metricName="areaUnderROC")
+
+
+### Make a grid
+
+# Import the tuning submodule
+import pyspark.ml.tuning as tune
+
+# Create the parameter grid
+grid = tune.ParamGridBuilder()
+
+# Add the hyperparameter
+grid = grid.addGrid(lr.regParam, np.arange(0, .1, .01))
+grid = grid.addGrid(lr.elasticNetParam, [0, 1])
+
+# Build the grid
+grid = grid.build()
+
+
+### Make the validator
+
+# Create the CrossValidator
+cv = tune.CrossValidator(estimator=lr,
+               estimatorParamMaps=grid,
+               evaluator=evaluator
+               )
+
+
+### Fit the model(s)
+
+# Call lr.fit()
+best_lr = lr.fit(training)
+
+# Print best_lr
+print(best_lr)
+
+
+### Evaluate the model
+
+# Use the model to predict the test set
+test_results = best_lr.transform(test)
+
+# Evaluate the predictions
+print(evaluator.evaluate(test_results))
