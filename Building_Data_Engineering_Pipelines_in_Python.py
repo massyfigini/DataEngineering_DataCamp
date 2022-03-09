035ddd6d75245b82b15f8af7936cb733bbb00f27
massyfigini <massimiliano.figini@gmail.com>
2022-03-09 18:18:11 +0100

    creating a data transformation pipeline with PySpark

--- Building_Data_Engineering_Pipelines_in_Python.py
+++ Building_Data_Engineering_Pipelines_in_Python.py
@@ -79,3 +79,109 @@ for shop in requests.get(SHOPS_URL).json()["shops"]:
       records=({**item, "store_name": shop}
                for item in retrieve_products(shop))
     )
+
+
+
+
+########################################################
+# Creating a data transformation pipeline with PySpark #
+########################################################
+
+### Reading a CSV file
+
+# Read a csv file and set the headers
+df = (spark.read
+      .options(header="true")
+      .csv("/home/repl/workspace/mnt/data_lake/landing/ratings.csv"))
+
+df.show()
+
+
+### Defining a schema
+
+# Define the schema
+schema = StructType([
+  StructField("brand", StringType(), nullable=False),
+  StructField("model", StringType(), nullable=False),
+  StructField("absorption_rate", ByteType(), nullable=True),
+  StructField("comfort", ByteType(), nullable=True)
+])
+
+better_df = (spark
+             .read
+             .options(header="true")
+             # Pass the predefined schema to the Reader
+             .schema(schema)
+             .csv("/home/repl/workspace/mnt/data_lake/landing/ratings.csv"))
+pprint(better_df.dtypes)
+
+
+### Removing invalid rows
+
+# Specify the option to drop invalid rows
+ratings = (spark
+           .read
+           .options(header=True, mode="DROPMALFORMED")
+           .csv("/home/repl/workspace/mnt/data_lake/landing/ratings_with_invalid_rows.csv"))
+ratings.show()
+
+
+### Filling unknown data
+
+print("BEFORE")
+ratings.show()
+
+print("AFTER")
+# Replace nulls with arbitrary value on column subset
+ratings = ratings.fillna(4, subset=["comfort"])
+ratings.show()
+
+
+### Conditionally replacing values
+
+from pyspark.sql.functions import col, when
+
+# Add/relabel the column
+categorized_ratings = ratings.withColumn(
+    "comfort",
+    # Express the condition in terms of column operations
+    when(col("comfort") > 3, "sufficient").otherwise("insufficient"))
+
+categorized_ratings.show()
+
+
+### Selecting and renaming columns
+
+from pyspark.sql.functions import col
+
+# Select the columns and rename the "absorption_rate" column
+result = ratings.select([col("brand"),
+                       col("model"),
+                       col("absorption_rate").alias("absorbency")])
+
+# Show only unique values
+result.distinct().show()
+
+
+### Grouping and aggregating data
+
+from pyspark.sql.functions import col, avg, stddev_samp, max as sfmax
+
+aggregated = (purchased
+              # Group rows by 'Country'
+              .groupBy(col('Country'))
+              .agg(
+                # Calculate the average salary per group and rename
+                avg('Salary').alias('average_salary'),
+                # Calculate the standard deviation per group
+                stddev_samp('Salary'),
+                # Retain the highest salary per group and rename
+                sfmax('Salary').alias('highest_salary')
+              )
+             )
+
+aggregated.show()
+
+
+
+
