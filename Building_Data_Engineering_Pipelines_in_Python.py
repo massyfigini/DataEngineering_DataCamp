0348ad47c1e491a6cddb794bcc11f65996c5205c
massyfigini <massimiliano.figini@gmail.com>
2022-03-24 10:27:58 +0100

    managing and orchestrating a workflow

--- Building_Data_Engineering_Pipelines_in_Python.py
+++ Building_Data_Engineering_Pipelines_in_Python.py
@@ -210,4 +210,98 @@ frame.show()
 
 
 
+######################################### 
+# Managing and orchestrating a workflow #
+#########################################
 
+### Specifying the DAG schedule
+
+from datetime import datetime
+from airflow import DAG
+
+reporting_dag = DAG(
+    dag_id="publish_EMEA_sales_report", 
+    # Insert the cron expression
+    schedule_interval="0 7 * * 1",
+    start_date=datetime(2019, 11, 24),
+    default_args={"owner": "sales"}
+)
+
+
+### Specifying operator dependencies
+
+# Specify direction using verbose method
+prepare_crust.set_downstream(apply_tomato_sauce)
+
+tasks_with_tomato_sauce_parent = [add_cheese, add_ham, add_olives, add_mushroom]
+for task in tasks_with_tomato_sauce_parent:
+    # Specify direction using verbose method on relevant task
+    apply_tomato_sauce.set_downstream(task)
+
+# Specify direction using bitshift operator
+tasks_with_tomato_sauce_parent >> bake_pizza
+
+# Specify direction using verbose method
+bake_pizza.set_upstream(prepare_oven)
+
+
+### Preparing a DAG for daily pipelines
+
+dag = DAG(
+  dag_id='optimize_diaper_purchases',
+  default_args={
+    # Don't email on failure
+    'email_on_failure': False,
+    # Specify when tasks should have started earliest
+    'start_date': datetime(2019, 6, 25)
+  },
+  # Run the DAG daily
+  schedule_interval='@daily')
+
+
+### Scheduling bash scripts with Airflow
+
+config = os.path.join(os.environ["AIRFLOW_HOME"], 
+                      "scripts",
+                      "configs", 
+                      "data_lake.conf")
+
+ingest = BashOperator(
+  # Assign a descriptive id
+  task_id="ingest_data", 
+  # Complete the ingestion pipeline
+  bash_command='tap-marketing-api | target-csv --config %s' % config,
+  dag=dag)
+
+
+### Scheduling Spark jobs with Airflow
+
+# Import the operator
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+
+# Set the path for our files.
+entry_point = os.path.join(os.environ["AIRFLOW_HOME"], "scripts", "clean_ratings.py")
+dependency_path = os.path.join(os.environ["AIRFLOW_HOME"], "dependencies", "pydiaper.zip")
+
+with DAG('data_pipeline', start_date=datetime(2019, 6, 25),
+         schedule_interval='@daily') as dag:
+  	# Define task clean, running a cleaning job.
+    clean_data = SparkSubmitOperator(
+        application=entry_point, 
+        py_files=dependency_path,
+        task_id='clean_data',
+        conn_id='spark_default')
+
+
+### Scheduling the full data pipeline with Airflow
+
+spark_args = {"py_files": dependency_path,
+              "conn_id": "spark_default"}
+# Define ingest, clean and transform job.
+with dag:
+    ingest = BashOperator(task_id='Ingest_data', bash_command='tap-marketing-api | target-csv --config %s' % config)
+    clean = SparkSubmitOperator(application=clean_path, task_id='clean_data', **spark_args)
+    insight = SparkSubmitOperator(application=transform_path, task_id='show_report', **spark_args)
+    
+    # set triggering sequence
+    ingest >> clean >> insight
