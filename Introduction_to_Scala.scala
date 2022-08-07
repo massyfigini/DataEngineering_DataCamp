8ee75c6b949bc3d25b3b091fcf6acfe8d265809e
massyfigini <massimiliano.figini@gmail.com>
2022-08-07 14:36:55 +0200

    type systems, control structures, style

--- Introduction_to_Scala.scala
+++ Introduction_to_Scala.scala
@@ -141,3 +141,111 @@ val venuesEuroTO = "Five Seasons Hotel" :: "The Electric Unicorn" :: Nil
 
 // Concatenate the North American and European venues
 val venuesTOWorld = venuesNTOA ::: venuesEuroTO
+
+
+
+
+/////////////////////////////////////////////
+// Type Systems, Control Structures, Style //
+/////////////////////////////////////////////
+
+////// if and printing
+
+// Point value of a player's hand
+val hand = sevenClubs + kingDiamonds + fourSpades
+
+// Congratulate the player if they have reached 21
+if (hand == 21) {
+   println("Twenty-One!")
+}
+
+
+////// if expressions result in a value
+
+// Point value of a player's hand
+val hand = sevenClubs + kingDiamonds + threeSpades
+
+// Inform a player where their current hand stands
+val informPlayer: String = {
+  if (hand > 21)
+    "Bust! :("
+  else if (hand == 21)
+    "Twenty-One! :)"
+  else
+    "Hit or stay?"
+}
+
+// Print the message
+print(informPlayer)
+
+
+////// if and else inside of a function
+
+// Find the number of points that will cause a bust
+def pointsToBust(hand: Int): Int = {
+  // If the hand is a bust, 0 points remain
+  if (bust(hand))
+    0
+  // Otherwise, calculate the difference between 21 and the current hand
+  else
+    (21-hand)
+}
+
+// Test pointsToBust with 10♠ and 5♣
+val myHandPointsToBust = pointsToBust(tenSpades+fiveClubs)
+println(myHandPointsToBust)
+
+
+////// A simple while loop
+
+// Define counter variable
+var i = 0
+
+// Define the number of loop iterations
+var numRepetitions = 3
+
+// Loop to print a message for winner of the round
+while (i < numRepetitions) {
+  if (i < 2)
+    println("winner")
+  else
+    println("chicken dinner")
+  // Increment the counter variable
+  i = i+1
+}
+
+
+////// Loop over a collection with while
+
+// Define counter variable
+var i = 0
+
+// Create list with five hands of Twenty-One
+var hands = List(16, 21, 8, 25, 4)
+
+// Loop through hands
+while (i < hands.length) {
+  // Find and print number of points to bust
+  println(pointsToBust(hands(i)))
+  // Increment the counter variable
+  i = i+1
+}
+
+
+////// Converting while to foreach
+
+// Find the number of points that will cause a bust
+def pointsToBust(hand: Int) = {
+  // If the hand is a bust, 0 points remain
+  if (bust(hand))
+    println(0)
+  // Otherwise, calculate the difference between 21 and the current hand
+  else
+    println(21 - hand)
+}
+
+// Create list with five hands of Twenty-One
+var hands = List(16, 21, 8, 25, 4)
+
+// Loop through hands, finding each hand's number of points to bust
+hands.foreach(pointsToBust)
