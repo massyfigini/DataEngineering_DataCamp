c96efa1f3c41b78cefb32fc35735055be0490b36
massyfigini <massimiliano.figini@gmail.com>
2022-10-09 17:05:28 +0200

    manipulating dataframes in the real world

--- Cleaning_Data_with_PySpark.py
+++ Cleaning_Data_with_PySpark.py
@@ -59,3 +59,129 @@ flights_df.createOrReplaceTempView('flights')
 # Run a SQL query of the average flight duration
 avg_duration = spark.sql('SELECT avg(flight_duration) from flights').collect()[0]
 print('The average flight time is: %d' % avg_duration)
+
+
+
+
+#############################################
+# Manipulating DataFrames in the real world #
+#############################################
+
+### Filtering column content with Python
+
+# Show the distinct VOTER_NAME entries
+voter_df.select('VOTER_NAME').distinct().show(40, truncate=False)
+
+# Filter voter_df where the VOTER_NAME is 1-20 characters in length
+voter_df = voter_df.filter('length(VOTER_NAME) > 0 and length(VOTER_NAME) < 20')
+
+# Filter out voter_df where the VOTER_NAME contains an underscore
+voter_df = voter_df.filter(~ F.col('VOTER_NAME').contains('_'))
+
+# Show the distinct VOTER_NAME entries again
+voter_df.select('VOTER_NAME').distinct().show(40, truncate=False)
+
+
+### Modifying DataFrame columns
+
+# Add a new column called splits separated on whitespace
+voter_df = voter_df.withColumn('splits', F.split(voter_df.VOTER_NAME, '\s+'))
+
+# Create a new column called first_name based on the first item in splits
+voter_df = voter_df.withColumn('first_name', voter_df.splits.getItem(0))
+
+# Get the last entry of the splits list and create a column called last_name
+voter_df = voter_df.withColumn('last_name', voter_df.splits.getItem(F.size('splits') - 1))
+
+# Drop the splits column
+voter_df = voter_df.drop('splits')
+
+# Show the voter_df DataFrame
+voter_df.show()
+
+
+### when() example
+
+# Add a column to voter_df for any voter with the title **Councilmember**
+voter_df = voter_df.withColumn('random_val',
+          when(voter_df.TITLE == 'Councilmember', F.rand()))
+          
+# Add a column to voter_df for a voter based on their position
+voter_df = voter_df.withColumn('random_val',
+    when(voter_df.TITLE == 'Councilmember', F.rand())
+    .when(voter_df.TITLE == 'Mayor', 2)
+    .otherwise(0))
+
+
+### When / Otherwise
+
+# Add a column to voter_df for a voter based on their position
+voter_df = voter_df.withColumn('random_val',
+                               when(voter_df.TITLE == 'Councilmember', F.rand())
+                               .when(voter_df.TITLE == 'Mayor', 2)
+                               .otherwise(0))
+
+# Show some of the DataFrame rows
+voter_df.show()
+
+# Use the .filter() clause with random_val
+voter_df.filter(voter_df.random_val == 0).show()
+
+
+### Using user defined functions in Spark
+
+def getFirstAndMiddle(names):
+  # Return a space separated string of names
+  return ' '.join(names[:-1])
+
+# Define the method as a UDF
+udfFirstAndMiddle = F.udf(getFirstAndMiddle, StringType())
+
+# Create a new column using your UDF
+voter_df = voter_df.withColumn('first_and_middle_name', udfFirstAndMiddle(voter_df.splits))
+
+# Show the DataFrame
+voter_df.show()
+
+
+### Adding an ID Field
+
+# Select all the unique council voters
+voter_df = df.select(df["VOTER NAME"]).distinct()
+
+# Count the rows in voter_df
+print("\nThere are %d rows in the voter_df DataFrame.\n" % voter_df.count())
+
+# Add a ROW_ID
+voter_df = voter_df.withColumn('ROW_ID', F.monotonically_increasing_id())
+
+# Show the rows with 10 highest IDs in the set
+voter_df.orderBy(voter_df.ROW_ID.desc()).show(10)
+
+
+### IDs with different partitions
+
+# Print the number of partitions in each DataFrame
+print("\nThere are %d partitions in the voter_df DataFrame.\n" % voter_df.rdd.getNumPartitions())
+print("\nThere are %d partitions in the voter_df_single DataFrame.\n" % voter_df_single.rdd.getNumPartitions())
+
+# Add a ROW_ID field to each DataFrame
+voter_df = voter_df.withColumn('ROW_ID', F.monotonically_increasing_id())
+voter_df_single = voter_df_single.withColumn('ROW_ID', F.monotonically_increasing_id())
+
+# Show the top 10 IDs in each DataFrame 
+voter_df.orderBy(voter_df.ROW_ID.desc()).show(10)
+voter_df_single.orderBy(voter_df_single.ROW_ID.desc()).show(10)
+
+
+### More ID tricks
+
+# Determine the highest ROW_ID and save it in previous_max_ID
+previous_max_ID = voter_df_march.select('ROW_ID').rdd.max()[0]
+ 
+# Add a ROW_ID column to voter_df_april starting at the desired value
+voter_df_april = voter_df_april.withColumn('ROW_ID', F.monotonically_increasing_id() + previous_max_ID)
+ 
+# Show the ROW_ID from both DataFrames and compare
+voter_df_march.select('ROW_ID').show()
+voter_df_april.select('ROW_ID').show()
