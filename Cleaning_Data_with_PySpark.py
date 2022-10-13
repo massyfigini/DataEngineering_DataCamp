ef5de5bc547bcf5ba38910c598737f9ff1121ad1
massyfigini <massimiliano.figini@gmail.com>
2022-10-13 21:25:34 +0200

    improving performance

--- Cleaning_Data_with_PySpark.py
+++ Cleaning_Data_with_PySpark.py
@@ -185,3 +185,126 @@ voter_df_april = voter_df_april.withColumn('ROW_ID', F.monotonically_increasing_
 # Show the ROW_ID from both DataFrames and compare
 voter_df_march.select('ROW_ID').show()
 voter_df_april.select('ROW_ID').show()
+
+
+
+
+#########################
+# Improving Performance #
+#########################
+
+### Caching a DataFrame
+
+start_time = time.time()
+ 
+# Add caching to the unique rows in departures_df
+departures_df = departures_df.distinct().cache()
+ 
+# Count the unique rows in departures_df, noting how long the operation takes
+print("Counting %d rows took %f seconds" % (departures_df.count(), time.time() - start_time))
+ 
+# Count the rows again, noting the variance in time of a cached DataFrame
+start_time = time.time()
+print("Counting %d rows again took %f seconds" % (departures_df.count(), time.time() - start_time))
+
+
+### Removing a DataFrame from cache
+
+# Determine if departures_df is in the cache
+print("Is departures_df cached?: %s" % departures_df.is_cached)
+print("Removing departures_df from cache")
+ 
+# Remove departures_df from the cache
+departures_df.unpersist()
+ 
+# Check the cache status again
+print("Is departures_df cached?: %s" % departures_df.is_cached)
+
+
+### File import performance
+
+# Import the full and split files into DataFrames
+full_df = spark.read.csv('departures_full.txt.gz')
+split_df = spark.read.csv('departures_*.txt.gz')
+ 
+# Print the count and run time for each DataFrame
+start_time_a = time.time()
+print("Total rows in full DataFrame:\t%d" % full_df.count())
+print("Time to run: %f" % (time.time() - start_time_a))
+ 
+start_time_b = time.time()
+print("Total rows in split DataFrame:\t%d" % split_df.count())
+print("Time to run: %f" % (time.time() - start_time_b))
+
+
+### Reading Spark configurations
+
+# Name of the Spark application instance
+app_name = spark.conf.get('spark.app.name')
+ 
+# Driver TCP port
+driver_tcp_port = spark.conf.get('spark.driver.port')
+ 
+# Number of join partitions
+num_partitions = spark.conf.get('spark.sql.shuffle.partitions')
+ 
+# Show the results
+print("Name: %s" % app_name)
+print("Driver TCP port: %s" % driver_tcp_port)
+print("Number of partitions: %s" % num_partitions)
+
+
+### Writing Spark configurations
+
+# Store the number of partitions in variable
+before = departures_df.rdd.getNumPartitions()
+ 
+# Configure Spark to use 500 partitions
+spark.conf.set('spark.sql.shuffle.partitions', 500)
+ 
+# Recreate the DataFrame using the departures data file
+departures_df = spark.read.csv('departures.txt.gz').distinct()
+ 
+# Print the number of partitions for each instance
+print("Partition count before change: %d" % before)
+print("Partition count after change: %d" % departures_df.rdd.getNumPartitions())
+
+
+### Normal joins
+
+# Join the flights_df and aiports_df DataFrames
+normal_df = flights_df.join(airports_df, \
+    flights_df["Destination Airport"] == airports_df["IATA"] )
+ 
+# Show the query plan
+normal_df.explain()
+
+
+### Using broadcasting on Spark joins
+
+# Import the broadcast method from pyspark.sql.functions
+from pyspark.sql.functions import broadcast
+ 
+# Join the flights_df and airports_df DataFrames using broadcasting
+broadcast_df = flights_df.join(broadcast(airports_df), \
+    flights_df["Destination Airport"] == airports_df["IATA"] )
+ 
+# Show the query plan and compare against the original
+broadcast_df.explain()
+
+
+### Comparing broadcast vs normal joins
+
+start_time = time.time()
+# Count the number of rows in the normal DataFrame
+normal_count = normal_df.count()
+normal_duration = time.time() - start_time
+ 
+start_time = time.time()
+# Count the number of rows in the broadcast DataFrame
+broadcast_count = broadcast_df.count()
+broadcast_duration = time.time() - start_time
+ 
+# Print the counts and the duration of the tests
+print("Normal count:\t\t%d\tduration: %f" % (normal_count, normal_duration))
+print("Broadcast count:\t%d\tduration: %f" % (broadcast_count, broadcast_duration))
